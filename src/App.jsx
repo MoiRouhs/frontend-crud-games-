1fd30d2003595f519f4b120664a46a94e929cfca
LuisRuiz9 <132725766+LuisRuiz9@users.noreply.github.com>
2024-11-22 18:39:22 -0500

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AllGames from './Componentes/AllGames/AllGames'
 import AddGame from './Componentes/AddGame/AddGame'
-import DeleteGame from './Componentes/DeleteGame/DeleteGame'
 import UpdateGame from './Componentes/UpdateGame/UpdateGame'
 import GameManagement from './Componentes/GameManagement/GameManagement'
 
@@ -17,7 +16,6 @@ function App() {
             <Route path="/" element={<GameManagement />} />
             <Route path='/allgames' element={<AllGames/>} />
             <Route path='/addgame' element={<AddGame />} />
-            <Route path='/deletegame' element={<DeleteGame />} />
             <Route path='/updategame' element={<UpdateGame />} />
 
           </Routes>
